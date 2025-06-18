47b87ae8329f61035957c92af05e9d45e35cf99a
mirror_sir <105334946@qq.com>
2025-06-18 23:29:53 +0800

    refactor(week1): Disable copy semantics for Timer class

--- week1/timer/Timer.h
+++ week1/timer/Timer.h
@@ -6,6 +6,8 @@ class Timer {
 public:
     Timer();
     ~Timer();
+    Timer(const Timer&) = delete;
+    Timer& operator=(const Timer&) = delete;
 private:
     std::chrono::time_point<std::chrono::high_resolution_clock> start_point_;
 };
